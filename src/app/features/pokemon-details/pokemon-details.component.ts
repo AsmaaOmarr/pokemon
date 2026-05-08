8ed8ff83910fb9440d927f526f345a39c412bd32
Asmaa Omar <omarasmaa241@gmail.com>
2026-05-08 17:19:06 +0300

    add back button

--- src/app/features/pokemon-details/pokemon-details.component.ts
+++ src/app/features/pokemon-details/pokemon-details.component.ts
@@ -1,13 +1,17 @@
 import { Component, computed, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PokemonDetailsStoreService } from '../../store/pokemon-details-store';
-
+import { NgIcon, provideIcons, provideNgIconsConfig } from "@ng-icons/core";
+import { heroArrowLeft } from '@ng-icons/heroicons/outline';
 @Component({
   selector: 'app-pokemon-details',
   templateUrl: './pokemon-details.component.html',
+  imports: [NgIcon],
+  providers: [provideIcons({ heroArrowLeft }), provideNgIconsConfig({ size: '1.2em' })],
 })
 export class PokemonDetailsComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
   private readonly pokemonDetailsStore = inject(PokemonDetailsStoreService);
   protected readonly pokemon = computed(() => this.pokemonDetailsStore.pokemonDetails());
   protected readonly infoCards = computed(() => {
@@ -34,5 +38,9 @@ export class PokemonDetailsComponent implements OnInit {
   ngOnInit() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pokemonDetailsStore.getPokemon(id);
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/']);
   }
 }
